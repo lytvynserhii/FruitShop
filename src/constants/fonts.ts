b6cd67cdedad198c2ec662eaf38263881eb9227d
Serhii Lytvyn <th.Serhii.Lytvyn@gmail.com>
2024-04-09 15:13:06 +0300

    feat: app fonts

---
+++ src/constants/fonts.ts
@@ -0,0 +1,12 @@
+export const MONTSERRAT = {
+  medium: 'Montserrat-Medium',
+  semiBold: 'Montserrat-SemiBold',
+  bold: 'Montserrat-Bold',
+};
+
+export const ROBOTO = {
+  light: 'Roboto-Light',
+  regular: 'Roboto-Regular',
+  medium: 'Roboto-Medium',
+  bold: 'Roboto-Bold',
+};
