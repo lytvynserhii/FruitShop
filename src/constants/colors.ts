c4ae39755350ea59299d154e3c253a2131b58d06
Serhii Lytvyn <th.Serhii.Lytvyn@gmail.com>
2024-04-09 14:49:41 +0300

    feat: app colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  black: '#000000',
+  blackWithOpacity: '#00000080',
+  white: '#ffffff',
+  lightGrey: '#C0C0C0',
+  lightGreyWithOpacity: '#C0C0C059',
+  grey: '#C4C4C4',
+  greyWithOpacity: '#C4C4C459',
+  yellow: '#F1C40F',
+  darkBlue: '#34495E',
+};
