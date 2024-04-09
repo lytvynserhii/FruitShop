c496b3ae209ddab2ad5afee7d73248f8348b64a4
Serhii Lytvyn <th.Serhii.Lytvyn@gmail.com>
2024-04-09 14:46:38 +0300

    build: set up navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 
+import { NavigationContainer } from '@react-navigation/native';
+
 import { ErrorBoundary } from 'components/ErrorBoundary/ErrorBoundary';
 
 import { store } from 'store/store';
@@ -8,7 +10,9 @@ import { store } from 'store/store';
 export const App = () => (
   <ErrorBoundary>
     <Provider store={store}>
-      <View />
+      <NavigationContainer>
+        <View />
+      </NavigationContainer>
     </Provider>
   </ErrorBoundary>
 );
