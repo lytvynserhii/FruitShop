0e9b4c5d2baddc1be01f1add4613e64fd69b7887
Serhii Lytvyn <th.Serhii.Lytvyn@gmail.com>
2024-04-09 13:39:08 +0300

    build: custom path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+        alias: {
+          assets: ['./src/assets'],
+          components: ['./src/components'],
+          constants: ['./src/constants'],
+          navigation: ['./src/navigation'],
+          screens: ['./src/screens'],
+          store: ['./src/store'],
+        },
+      },
+    ],
+  ],
 };
