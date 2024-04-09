f8aa87f3215ed25946cc5345a20987b6f8e195af
Serhii Lytvyn <th.Serhii.Lytvyn@gmail.com>
2024-04-09 15:39:21 +0300

    feat: project images

---
+++ src/constants/images.ts
@@ -0,0 +1,21 @@
+export const ONBOARDING_IMAGES = {
+  slide1: require('assets/images/onboarding/onboarding-slide1.png'),
+  slide2: require('assets/images/onboarding/onboarding-slide2.png'),
+  slide3: require('assets/images/onboarding/onboarding-slide3.png'),
+};
+
+export const HEADER_IMAGES = {
+  logo: require('assets/images/header/logo/logo.png'),
+  profile: require('assets/images/header/profile/profile.png'),
+};
+
+export const FRUIT_IMAGES = {
+  banana: require('assets/images/fruit/banana/banana.png'),
+  greenGrape: require('assets/images/fruit/green-grape/green-grape.png'),
+  guava: require('assets/images/fruit/guava/guava.png'),
+  mango: require('assets/images/fruit/mango/mango.png'),
+  melon: require('assets/images/fruit/melon/melon.png'),
+  pineapple: require('assets/images/fruit/pineapple/pineapple.png'),
+  pitaya: require('assets/images/fruit/pitaya/pitaya.png'),
+  purpleGrape: require('assets/images/fruit/purple-grape/purple-grape.png'),
+};
