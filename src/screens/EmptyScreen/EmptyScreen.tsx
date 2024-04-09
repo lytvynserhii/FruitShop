dcf13a5457e73381c1220ca83dca257eeeb9c9b7
Serhii Lytvyn <th.Serhii.Lytvyn@gmail.com>
2024-04-09 14:49:52 +0300

    feat: empty screen

---
+++ src/screens/EmptyScreen/EmptyScreen.tsx
@@ -0,0 +1,5 @@
+import { SafeAreaView } from 'react-native';
+
+import { styles } from './emptyScreen.styles';
+
+export const EmptyScreen = () => <SafeAreaView style={styles.safeAreaView} />;
---
+++ src/screens/EmptyScreen/emptyScreen.styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+import { COLORS } from 'constants/colors';
+
+export const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+});
