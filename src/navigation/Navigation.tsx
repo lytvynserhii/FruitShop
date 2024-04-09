fa62342153e7b0acd81cd21e05ac1ee6fe18948f
Serhii Lytvyn <th.Serhii.Lytvyn@gmail.com>
2024-04-09 16:09:42 +0300

    feat: fill app stack navigator with screens

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,7 +1,10 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { EmptyScreen } from 'screens/EmptyScreen/EmptyScreen';
+import { LoginScreen } from 'screens/AuthScreens/LoginScreen/LoginScreen';
+import { RegistrationScreen } from 'screens/AuthScreens/RegistrationScreen/RegistrationScreen';
+import { OnboardingScreen } from 'screens/OnboardingScreen/OnboardingScreen';
 
+import { TabNavigator } from './TabNavigator';
 import { ROOT_STACK_OPTIONS } from './navigationOptions';
 
 import { RootStackParamList } from './navigation.types';
@@ -10,9 +13,9 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const AppStack = () => (
   <Stack.Navigator screenOptions={ROOT_STACK_OPTIONS} initialRouteName="Login">
-    <Stack.Screen name="Login" component={EmptyScreen} />
-    <Stack.Screen name="Registration" component={EmptyScreen} />
-    <Stack.Screen name="Onboarding" component={EmptyScreen} />
-    <Stack.Screen name="Tabs" component={EmptyScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Registration" component={RegistrationScreen} />
+    <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+    <Stack.Screen name="Tabs" component={TabNavigator} />
   </Stack.Navigator>
 );
